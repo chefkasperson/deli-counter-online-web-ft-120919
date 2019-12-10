6244bfbe915966c4e9f50dc870cb9f91dd16a646
Erik Kasperson <chefkasperson@gmail.com>
2019-12-10 15:20:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    x = array.map.with_index(1)
-    x = x.join(" ")
-    puts "The line is currently:" + x
-    binding.pry
+    counter = 1
+    x = array.map.with_index(1) {|person, i| " #{i}. #{person} "}
+    y = x.join
+    puts "The line is currently:" + y
   end
 end
